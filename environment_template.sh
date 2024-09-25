d83c145486f65733c12e36c16b6d4b7e398049ef
Tom Byers <tombaromba@gmail.com>
2024-09-25 09:37:44 +0100

    Add template for environment variables

---
+++ environment_template.sh
@@ -0,0 +1,7 @@
+export TEST_NUMBER=''
+export FUNCTIONAL_TESTS_SERVICE_INBOUND_NUMBER=''
+export FUNCTIONAL_TESTS_API_HOST=''
+export MMG_INBOUND_SMS_USERNAME= # 'username' value for Development config in notifications-api
+export MMG_INBOUND_SMS_AUTH='' # 'testkey' value for Development config in notifications-api
+export SERVICE_API_KEY=''
+export SEND_FILE_BY_EMAIL_TEMPLATE_ID=''
